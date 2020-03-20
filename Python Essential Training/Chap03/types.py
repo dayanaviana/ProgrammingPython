dd60ee9126961eba38cfa9e31ff44bcb4b7ab929
Dayana Viana <dayanatsv@gmail.com>
2020-03-19 22:39:26 -0600

    Adding exercise files

---
+++ Python Essential Training/Chap03/types.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+# x = 7
+# print('x is {}'.format(x))
+# print(type(x))
+
+# x = '''
+
+# seven
+
+# ''' #multiline string
+# print('x is {}'.format(x))
+# print(type(x))
+
+# x = "seven".capitalize()
+# print('x is {}'.format(x))
+# print(type(x))
+
+# x = "SEVEN".lower()
+# print('x is {}'.format(x))
+# print(type(x))
+
+# x = "seven {} {}".format(8,9)
+# print('x is {}'.format(x))
+# print(type(x))
+
+# x = "seven {1} {0}".format(8,9)
+# print('x is {}'.format(x))
+# print(type(x))
+
+# ## Fills 9 spaces after #1 and 9 before #0
+# x = "seven '{1:<9}' '{0:>9}'.".format(8,9)
+# print('x is {}'.format(x))
+# print(type(x))
+
+# x = "seven '{1:<09}' '{0:>09}'.".format(8,98798798)
+# print('x is {}'.format(x))
+# print(type(x))
+
+a =8
+b=9
+x = f"seven {a} {b}"
+print('x is {}'.format(x))
+print(type(x))
